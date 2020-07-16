418c67c299ae793f214b83b8401eb11d4dfbbb99
Xeinia Bastrakova <bastrakova.kseinia@gmail.com>
2020-07-16 12:08:30 +0200

    delete untraked files

--- reduction_template_library/Debug/src/openPMDreadingSample.d
+++
@@ -1,9 +0,0 @@
-src/openPMDreadingSample.o: ../src/openPMDreadingSample.cpp \
- ../src/Attributes_getting.hpp ../src/Particle_type.hpp \
- ../src/Atributes_types.hpp
-
-../src/Attributes_getting.hpp:
-
-../src/Particle_type.hpp:
-
-../src/Atributes_types.hpp:
