b660eab721c37ef8911eff2c8f99c1904a3d570a
Xeinia Bastrakova <bastrakova.kseinia@gmail.com>
2020-07-16 11:42:50 +0200

    delete extra dir

---
+++ src/attribute/Type.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include "Traits.hpp"
+
+namespace reduction_library{
+namespace attribute{
+
+template<typename T_Particle, Attribute T_attribute>
+using Type = typename traits::Type<T_Particle, T_attribute>::type;
+
+
+
+    }
+}
