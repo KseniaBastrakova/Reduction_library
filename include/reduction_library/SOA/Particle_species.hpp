ed029929dc12bef2a7b9ba7a2e6cc1c449de7ccc
Sergei Bastrakov <sergey.bastrakov@gmail.com>
2020-08-19 15:30:14 +0200

    Add generic species prototype

--- include/reduction_library/SOA/Particle_species.hpp
+++ include/reduction_library/SOA/Particle_species.hpp
@@ -13,12 +13,12 @@ namespace SOA{
     template <typename T_First_record, typename T_Second_record>
     class Particle_species{
         int size;
-        T_First_record first_record;
-        T_Second_record second_record;
+     //   T_First_record first_record;
+     //   T_Second_record second_record;
     public:
         using MyParticle = Particle<Particle_species>;
-  //      using Records = std::tuple<T_First_record, T_Second_record>;
-     //   Records records;
+        using Records = std::tuple<T_First_record, T_Second_record>;
+        Records records;
         Particle_species(T_First_record first_record, T_Second_record second_record):
             first_record(first_record), second_record(second_record)
         {
@@ -32,6 +32,15 @@ namespace SOA{
             return size;
         }
 
+        /// Temporary
+        template< record::Name T_record_name >
+        constexpr static size_t getIndex()
+        {
+            return T_record_name == record::Name::momentum ? 0 :
+                T_record_name == record::Name::weighting ? 1 : 2;
+        }
+
+
     };
 
 } // SOA
@@ -39,44 +48,29 @@ namespace SOA{
 namespace particle_species{
 namespace traits{
 
-    template<typename T_First_record, typename T_Second_record>
-    struct Type<record::Name::momentum, SOA::Particle_species<T_First_record,T_Second_record>>
+    template<record::Name T_record_name, typename T_First_record, typename T_Second_record>
+    struct Type<T_record_name, SOA::Particle_species<T_First_record,T_Second_record>>
     {
-        using type = T_First_record;
-    };
-
-    template<typename T_First_record, typename T_Second_record>
-    struct Type<record::Name::weighting, SOA::Particle_species<T_First_record,T_Second_record>>
-    {
-        using type = T_Second_record;
+        using type = std::tuple_element_t<
+            SOA::Particle_species<T_First_record,T_Second_record>::getIndex< T_record_name >(),
+            SOA::Particle_species<T_First_record,T_Second_record>::Records
+         >;
     };
 
 } // namespace traits
 
-template<typename T_First_record, typename T_Second_record>
-struct Getting_value<record::Name::momentum, SOA::Particle_species<T_First_record,T_Second_record>>
+template<record::Name T_record_name, typename T_First_record, typename T_Second_record>
+struct Getting_value<T_record_name, SOA::Particle_species<T_First_record,T_Second_record>>
 {
 public:
-    typename traits::Type<record::Name::momentum, SOA::Particle_species<T_First_record,T_Second_record>>::type
-        operator() (SOA::Particle_species<T_First_record,T_Second_record>& particle_species)
+    auto operator() (SOA::Particle_species<T_First_record,T_Second_record>& particle_species)
     {
-        return particle_species.first_record;
+        constexpr auto idx = SOA::Particle_species<T_First_record,T_Second_record>::getIndex< T_record_name >();
+        return std::get< idx >( particle_species.records );
     }
 
 };
 
-
-template<typename T_First_record, typename T_Second_record>
-struct Getting_value<record::Name::weighting, SOA::Particle_species<T_First_record,T_Second_record>>
-{
-public:
-    typename traits::Type<record::Name::weighting, SOA::Particle_species<T_First_record,T_Second_record>>::type
-        operator() (SOA::Particle_species<T_First_record,T_Second_record>& particle_species)
-    {
-        return particle_species.second_record;
-    }
-
-};
 
 } // namespace particle_spicies
 } // reduction_library
